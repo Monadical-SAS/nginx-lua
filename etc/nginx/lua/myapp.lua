6e9afdc48151477030e469fbe89c5bc2ee2ac90a
apkallum <apkallum@protonmail.com>
2019-09-24 20:34:52 -0400

    add lua file in openresty

--- etc/nginx/lua/myapp.lua
+++ etc/nginx/lua/myapp.lua
@@ -1,6 +1,5 @@
 local template = require("resty.template")
-local template_string = ngx.location.capture("/templates/greet.html")
+local template_string = ngx.location.capture("/trade.html")
 template.render(template_string.body, {
-    param1 = ngx.req.get_uri_args()["param1"],
-    param2 = ngx.req.get_uri_args()["param2"]
+    id = ngx.req.get_uri_args()["id"]
 })
